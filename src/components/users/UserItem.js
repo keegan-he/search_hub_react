62f765d2d1eeb1e709e16066c8f39794a3cd83c7
Keegan H <keeganslane@gmail.com>
2019-06-24 21:01:07 -0700

    hard coded sample data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'keegan-he',
+      avatar_ural:
+        'https://avatars1.githubusercontent.com/u/46201161?s=460&v=4',
+      html_url: 'https://github.com/keegan-he'
+    };
+  }
   render() {
     return <div>UserItem</div>;
   }
