59669003af0eed050afe655d43f71e9c9b618c88
Keegan H <keeganslane@gmail.com>
2019-06-24 21:10:55 -0700

    removed super constructor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'keegan-he',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/46201161?s=460&v=4',
-      html_url: 'https://github.com/keegan-he'
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'keegan-he',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/46201161?s=460&v=4',
+    html_url: 'https://github.com/keegan-he'
+  };
+
   render() {
     return (
       <div className="card text-center">
